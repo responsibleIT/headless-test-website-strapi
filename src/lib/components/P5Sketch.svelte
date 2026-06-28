45001a09d0a1e396f6faf274e33fc921649a440b
Xue_Dragon <159780673+XueDragon@users.noreply.github.com>
2026-06-28 07:24:42 +0200

    Added P5 en documentatie

---
+++ src/lib/components/P5Sketch.svelte
@@ -0,0 +1,36 @@
+<script>
+	let { code } = $props();
+
+	let iframeHtml = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<style>
+body {
+	margin: 0;
+	overflow: hidden;
+}
+canvas {
+	display: block;
+}
+</style>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.4/p5.min.js"><\/script>
+</head>
+<body>
+<script>
+${code ?? ''}
+<\/script>
+</body>
+</html>
+`;
+</script>
+
+<iframe
+	title="P5 Sketch"
+	srcdoc={iframeHtml}
+	sandbox="allow-scripts"
+	style="width:100%; height:800px; border:none;"
+></iframe>
